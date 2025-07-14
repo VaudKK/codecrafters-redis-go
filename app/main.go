684aa4f0a9ed3521fe8bb8e2c08e45f6234ce2a0
VaudKK <vaudkeith@gmail.com>
2025-07-14 14:01:05 +0300

    implementing echo

--- app/main.go
+++ app/main.go
@@ -41,7 +41,7 @@ func handleConnection(connection net.Conn) {
 
 	for scanner.Scan() {
 		line := scanner.Text()
-		request += line + "\r\n"
+		request += fmt.Sprintf("%s\r\n", line)
 	}
 
 	handle(request, connection)
