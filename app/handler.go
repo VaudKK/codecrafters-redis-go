093193ba832513d4d608729d98b64cc0534691bd
VaudKK <vaudkeith@gmail.com>
2025-07-15 23:38:00 +0300

    debugging

--- app/handler.go
+++ app/handler.go
@@ -9,7 +9,11 @@ import (
  var Handler = map[string] func([]string,net.Conn){
 	"PING": ping,
 	"ECHO": echo,
+	"SET":  set,
+	"GET":  get,
 }
+
+var keyValue = make(map[string]string)
 
 
 func echo(tokens []string, connection net.Conn) {
@@ -22,4 +26,25 @@ func echo(tokens []string, connection net.Conn) {
 
 func ping(tokens []string,connection net.Conn) {
 	connection.Write([]byte("+PONG\r\n"))
+}
+
+func set(tokens []string, connection net.Conn) {
+	_,ok := keyValue[tokens[1]]
+
+	if !ok {
+		keyValue[tokens[1]] = tokens[2]
+	}
+
+	connection.Write([]byte("+OK\r\n"))
+}
+
+func get(tokens []string, connection net.Conn) {
+	value, ok := keyValue[tokens[1]]
+	if !ok {
+		connection.Write([]byte("$-1\r\n"))
+		return
+	}
+
+	response := fmt.Sprintf("$%d\r\n%s\r\n", len(value), value)
+	connection.Write([]byte(response))
 }
