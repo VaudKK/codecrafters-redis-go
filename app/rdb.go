33268236dd4cb72cec3ed00bb44f31521b60c287
VaudKK <vaudkeith@gmail.com>
2025-07-24 00:28:15 +0300

    read key

--- app/rdb.go
+++ app/rdb.go
@@ -77,12 +77,12 @@ func readByte(fileData []byte) byte {
 
 func readBytesOffset(fileData []byte,offset int, length int) []byte{
 	destination := fileData[offset:(offset + length)]
-	pos += length
+	pos += length - 1
 	return destination
 }
 
 func readBytes(fileData []byte,length int) []byte{
 	value := fileData[pos:(pos + length)]
-	pos += length
+	pos += length -1
 	return value
 }
