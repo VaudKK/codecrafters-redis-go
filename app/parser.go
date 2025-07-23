1df2a647a9ee1ce09dc074d5896acce5d616e1bb
VaudKK <vaudkeith@gmail.com>
2025-07-23 22:32:04 +0300

    read key

--- app/parser.go
+++ app/parser.go
@@ -46,12 +46,12 @@ func parse(respType RESPType,tokens []string, connection net.Conn) {
 func parseArray(tokens []string) []string {
 	data := make([]string, 0)
 
-	for _, token := range tokens {
+	for index, token := range tokens {
 		if strings.HasPrefix(token, "$") {
 			continue
 		}
 
-		if strings.HasPrefix(token, "*") {
+		if(strings.HasPrefix(token, "*") && index > 0) {
 			continue
 		}
 
